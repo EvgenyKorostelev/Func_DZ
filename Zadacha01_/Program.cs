8ffedb37c3a6ab4015f971bb22a1883ac7a9ed06
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-29 11:21:42 +0300

    add and reshil 1 zadachu v Zadacha01_

--- Zadacha01_/Program.cs
+++ Zadacha01_/Program.cs
@@ -3,22 +3,17 @@
 
 void Func(string num)
 {
-    int result = 1;
-    while(num != "q" | result != 0)
+    
+    while(num != "q")
     {
-        Console.Write("Введите целое число: ");
-        string numS = Console.ReadLine();
-        Console.WriteLine();
-        num = numS;
-                
-        int numI = Convert.ToInt32(numS);
+        int numI = Convert.ToInt32(num);
         int numTTT = numI;
         int count = 0;
         while (numTTT != 0)
-          {
+        {
             numTTT = numTTT / 10;
             count++;
-          }
+        }
         
         int[] array = new int[count];
         for (int i = 0; i < count; i++)
@@ -26,23 +21,31 @@ void Func(string num)
             array[i] = numI % 10;
             numI /= 10;
         }
+        Console.WriteLine();
 
-        for(int j = count - 1; j >= 0; j--)
-        {
-            Console.Write(array[j] + " ");
-        }
-        
         int resulTT = 0;
         for(int m = 0; m < count; m++)
         {
             resulTT = resulTT + array[m];
         }
+        Console.WriteLine($"Сумма цифр числа {num} равна: {resulTT}");
+        
+        int result = 1;
         result = resulTT % 2;
+        if (result == 0)
+        {
+            break;
+        }
+        Console.WriteLine();
+        Console.Write("Введите целое число: ");
+        string numS = Console.ReadLine();
+        num = numS;
+        
     }
-
+    Console.WriteLine("[STOP]");
 }
 
 Console.Write("Введите целое число: ");
-string numV = (Console.ReadLine());
+string numV = Console.ReadLine();
 
 Func(numV);
